eea3cf33d5a9c2b49cb01a3dc464e9eb7d1b1a32
Tangui Clairet <tangui.clairet@gmail.com>
2022-01-12 11:51:15 +0100

    export CalculateMinFee

--- tx.go
+++ tx.go
@@ -189,10 +189,10 @@ func (body *TransactionBody) AddSignatures(publicKeys [][]byte, signatures [][]b
 }
 
 func (body *TransactionBody) HasBurnedInput() bool {
-	return body.calculateMinFee() != body.Fee
+	return body.CalculateMinFee() != body.Fee
 }
 
-func (body *TransactionBody) calculateMinFee() uint64 {
+func (body *TransactionBody) CalculateMinFee() uint64 {
 	fakeXSigningKey := crypto.NewExtendedSigningKey([]byte{
 		0x0c, 0xcb, 0x74, 0xf3, 0x6b, 0x7d, 0xa1, 0x64, 0x9a, 0x81, 0x44, 0x67, 0x55, 0x22, 0xd4, 0xd8, 0x09, 0x7c, 0x64, 0x12,
 	}, "")
@@ -213,7 +213,7 @@ func (body *TransactionBody) calculateMinFee() uint64 {
 func (body *TransactionBody) addFee(inputAmount uint64, changeAddress Address) error {
 	// Set a temporary fee in order to serialize a valid transaction
 	body.Fee = maxUint64
-	minFee := body.calculateMinFee()
+	minFee := body.CalculateMinFee()
 
 	outputAmount := uint64(0)
 	for _, txOut := range body.Outputs {
@@ -240,7 +240,7 @@ func (body *TransactionBody) addFee(inputAmount uint64, changeAddress Address) e
 		Address: changeAddress.Bytes(),
 		Amount:  change, // set a temporary value
 	}}, body.Outputs...) // change will always be outputs[0] if present
-	newMinFee := body.calculateMinFee()
+	newMinFee := body.CalculateMinFee()
 	body.Outputs[0].Amount = change + minFee - newMinFee
 	body.Fee = newMinFee
 
